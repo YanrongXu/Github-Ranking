adf542d0c689945e18a734d5f2e079a6204c8550
Danny Xu <xuyanrong@icloud.com>
2020-11-17 13:53:38 -0500

    Create Dynamic top Navigation

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -2,23 +2,42 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
 import {createAppContainer} from 'react-navigation';
+import NavigationUtil from '../navigator/NavigationUtil';
 
 export default class PopularPage extends Component{
+    constructor(props){
+        super(props);
+        this.tabNames = ['Java', 'Android', 'IOS', 'React', 'React Native', 'PHP']
+    }
+    _genTabs () {
+        const tabs={};
+        this.tabNames.forEach((item, index) => {
+            tabs[`tab${index}`]={
+                screen: props => <PopularTab {...props} tabLabel={item} />,
+                navigationOptions: {
+                    title: item,
+
+                }
+            }
+        })
+        return tabs;
+    }
     render() {
-        const TabNavigator = createAppContainer(createMaterialTopTabNavigator({
-            popularTab_1: {
-                screen: PopularTab,
-                navigationOptions: {
-                    title: 'Tab1',
-                },
-            },
-            popularTab_2: {
-                screen: PopularTab,
-                navigationOptions: {
-                    title: 'Tab2',
-                },
-            },
-        }))
+        const TabNavigator = createAppContainer(createMaterialTopTabNavigator(
+            this._genTabs(),
+            {
+                tabBarOptions: {
+                    tabStyle: styles.tabStyle,
+                    upperCaseLabel: false,
+                    scrollEnabled: true,
+                    style: {
+                        backgroundColor: '#a67',
+                    },
+                    indicatorStyle: styles.indicatorStyle,
+                    labelStyle: styles.labelStyle
+                }
+            }
+        ))
 
         return(
             <View style={styles.container}>
@@ -32,6 +51,9 @@ class PopularTab extends Component {
         return (
             <View style={styles.container}>
                 <Text>PopularTab</Text>
+                <Text onPress={() => {
+                    NavigationUtil.goPage({}, 'DetailPage')
+                }}>Go to detail page</Text>
             </View>
         )
     }
@@ -46,5 +68,17 @@ const styles = StyleSheet.create({
         fontSize: 20, 
         textAlign: 'center',
         margin: 10,
+    },
+    tabStyle: {
+        minWidth: 50
+    },
+    indicatorStyle: {
+        height: 2,
+        backgroundColor: 'white'
+    },
+    labelStyle: {
+        fontSize: 13,
+        marginTop:6,
+        marginBottom: 6
     }
 })
