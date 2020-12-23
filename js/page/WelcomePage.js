d8abcbbc3e59a305ffed061bf6910395bdccb518
YanrongXu <xuyanrong@icloud.com>
2020-12-23 07:26:13 -0500

    add launch screen to the app

--- js/page/WelcomePage.js
+++ js/page/WelcomePage.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import NavigationUtil from '../navigator/NavigationUtil';
+import SplashScreen from 'react-native-splash-screen'
 
 export default class WelcomePage extends Component{
     componentDidMount() {
         this.timer = setTimeout(() => {
-            // jump to homepage
-            NavigationUtil.resetToHomePage(this.props)
-        }, 2000);
+            SplashScreen.hide()
+            NavigationUtil.resetToHomePage({
+                navigation: this.props.navigation
+            })
+        }, 400);
     }
     componentWillMount() {
         // when page jump, clear the timer
